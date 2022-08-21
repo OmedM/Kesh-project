0f75d611c1a544609d09b5b75868ba6062eafcfb
OmedM <110693726+OmedM@users.noreply.github.com>
2022-08-21 09:28:18 +0300

    API responding

--- src/redux/weather-slice.js
+++ src/redux/weather-slice.js
@@ -1,18 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const weatherSlice = createSlice({
     name: 'weather',
     initialState: {
         APIkey: '9b766b252617b12c13adf366d8702256',
-        weatherData: {}
+        weatherData: []
     },
     reducers: {
         newData(state, action) {
-            state.weatherData = fetch(
-                `api.openweathermap.org/data/2.5/forecast?q=${action.payload}&appid=${state.APIkey}`
-            );
+            const API = `https://api.openweathermap.org/data/2.5/forecast?q=${action.payload}&appid=${state.APIkey}`;
+
+            axios.get(API).then((response) => { console.log(response.data) });
         
-            console.log(state.weatherData);
+            
         }
     }
 });
